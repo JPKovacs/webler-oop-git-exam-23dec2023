8d8db276d1dd8ed66b43051a129f700186f2836a
Janos Kovacs <janos.kovacs@hotmail.hu>
2023-12-23 09:01:37 +0100

    Feature branch commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,15 @@ public class Main {
         Animals myAnimal = new Animals();
         Dog myDog = new Dog();
         Cat myCat = new Cat();
+        Animals myAnimal2 = new Animals();
+        Dog myDog2 = new Dog();
+        Cat myCat2 = new Cat();
         myAnimal.makeSound();
         myDog.bark();
         myCat.meow();
+        myAnimal2.makeSound();
+        myDog2.bark();
+        myCat2.meow();
     }
 
 }
